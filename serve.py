f5628b9ce95ee82ac4503676e08909e86d3bc4bc
Md. Tahmidur Rahman <tahmidullas@gmail.com>
2019-07-04 13:45:36 +0600

    Update serve.py

--- serve.py
+++ serve.py
@@ -1,24 +1,33 @@
 from flask import Flask, request, Response
 import jsonpickle
+import json
 import numpy as np
 import cv2
 
 app = Flask(__name__)
 
-@app.route('/api/testApi', methods=['POST'])
-def post():
+@app.route('/api/testApi/<mailId>')
+def get(mailId):
     req = request
     npArr = np.fromstring(req.data, np.uint8)
     
     img = cv2.imdecode(npArr, cv2.IMREAD_COLOR)
-
     i=SessionCount()
-    cv2.imwrite('C:/Users/TRU55/Desktop/Saved_Folder/ApiHandledCopys{}.jpg'.format(i),img)
+    cv2.imwrite(os.getcwd()+'/Saved_Folder/ApiHandledCopys{}.jpg'.format(i),img)
     cv2.waitKey(0)
+    
+    from mailToClient import User
+    mail=User();
+    #mail.addHeader("tahmidullas@gmail.com", "test mail")
+    mail.addHeader(mailId, "test mail")
+    mail.addBody("test mail body.")
+    filename = os.getcwd()+"/Saved_Folder/ApiHandledCopys{}.jpg".format(i)
+    #attachment = open("C:/Users/TRU55/Desktop/Saved_Folder/", "rb")
+    mail.addAttachment(filename)
+    mail.sendToClient()
     # build a response dict to send back to client
-    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
-                }
-    # encode response using jsonpickle
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])}
+    # encode response using jsonpickleC  
     resp = jsonpickle.encode(response)
 
     return Response(response=resp, status=200, mimetype="application/json")
