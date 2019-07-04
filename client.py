78c171fdf49b9fecab9e1827f9b895dc36818137
Md. Tahmidur Rahman <tahmidullas@gmail.com>
2019-07-04 13:50:18 +0600

    Update client.py

--- client.py
+++ client.py
@@ -4,14 +4,14 @@ import cv2
 
 def post_Image():
 	url = 'http://localhost:5000'
-	test_url = url + '/api/testApi'
-	content_type = 'image/jpeg'
-	headers = {'content-type': content_type}
-	images=['demo1.jpg','demo2.jpg','demo3.jpg']
-	for i in range(len(images)):
-		img = cv2.imread(images[i])
-		_, img_encoded = cv2.imencode('.jpg', img)
-		resp = requests.post(test_url, data=img_encoded.tostring(), headers=headers)
-		print(json.loads(resp.text))
+	mailId=['example@Gmail.com','example@yahoo.com']  #Array of mail Id-s to mail the images to a specific user
+	for item in mailId:	
+		test_url = url + '/api/testApi/'+item  #endpoint with query param
+		images=['fatbot.jpg']
+		for i in range(len(images)):
+			img = cv2.imread(images[i])
+			_, img_encoded = cv2.imencode('.jpg', img)
+			resp = requests.request(method='GET',url=test_url,data=img_encoded.tostring())  # to get response 
+			print(json.loads(resp.text))
 
 post_Image()
